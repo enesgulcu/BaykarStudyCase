98f8e784f004c18284edcca59bfe21066ac59f6c
Enes Gülcü <44102048+enesgulcu@users.noreply.github.com>
2022-12-20 11:32:47 +0300

    Study Case Complated Successfully.

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 ## Giriş Notu
+(frontend CSS) => %96
+(javascript Data Table) => %98
+
 Verilen Study Case için istenilen süreç içerisinde talimatlara göre düzenlemeler yapılmıştır.
-Aktif çalışma durumumdan dolayı proje zaman yetersizliğinden dolayı tam olarak bitirilememiştir.
-(frontend CSS) => %95
-(javascript Data Table) => %75
+Aktif çalışma durumumdan dolayı proje zaman yetersizliğinden dolayı plansız ve hızlı kodlanmıştır.
+
 
 ## Kişisel Yorum
-istenilen özelliklerin hepsini yapılabilir düzeyde. ancak daha fazla süre olsaydı daha kaliteli içerik hazırlanabilirdi ve istenilen yapılar tamamlanabilirdi.
-çalışma durumundan dolayı yetersiz zaman; hızlı kod yazmaya ve verimsiz planlamaya sebep olmuştur. Yapıtığım işten memnunum.  Daha iyi ve daha düzenli sistematik yapılar hazırlanabilirdi ancak süreden dolayı tam olarak tamamlanamadı.
+Daha kaliteli içerik, planlanmş temiz kod hazırlanabilirdi.
+çalışma durumundan dolayı yetersiz zaman; hızlı kod yazmaya ve verimsiz planlamaya sebep olmuştur. Yapıtığım işten memnunum. istenilenlerin %97'ini yaptığımı düşünüyorum.  Daha iyi ve daha düzenli sistematik yapılar hazırlanabilirdi ancak süreden dolayı kodlar biraz spagetti olabilir.
 
 ## Proje Özet
 proje Nex.js ile kodlanmıştır.
+veriler güncellenebilir - silinebilir - guruplanabilir - aramaya göre listelenebilir.
+tüm guruplama - listeleme - arama işlemlerine göre CSV çıktı alınabilir.
 mobil - tablet - bilgisayar - televizyon ölçülerine uyumlu tam responsive tasarlanmıştır.
 API ile veri çekilmiş, çekilen veriler filtrelenerek listelenmiştir. 
-CSV çıktı alınabilir.
+
+## ENES GÜLCÜ
+medyanestv@gmail.com
 
 
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -9,6 +9,8 @@ export default function Dashboard() {
   const [paginationStart, setPaginationStart] = useState(0);
   const [paginationEned, setPaginationEned] = useState(10);
   const [search, setSearch] = useState('');
+  const [groupData, setGroupData] = useState([]);
+  const [editIndex, setEditIndex] = useState([]);
 
   const pagination = (purecess) => {
     if (purecess === "next" && paginationEned < filterData.length) {
@@ -19,6 +21,38 @@ export default function Dashboard() {
       setPaginationEned(paginationEned - 10);
     }
   };
+
+  const edit = (e, selectIndex) => {
+    filterData?.map((item, index) => {
+      if (index === selectIndex) {
+        setEditIndex({
+          name: item.name.common,
+          capital: item.capital,
+          flags: item.flags.png,
+          population: item.population,
+          region: item.region,
+          borders: item.borders,
+          maps:item.maps.googleMaps
+
+        })
+      }
+    });
+    
+  }
+
+
+  const group = () => {
+    let selectedCheckBox = [...document.querySelectorAll('input[type="checkbox"]:checked')].map((item) => item.value);
+    setGroupData(selectedCheckBox);
+
+    const results = allData.filter((item) => {
+      return selectedCheckBox.includes(item.name.common);
+    });
+    setFilterData(results);
+    setPaginationStart(0);
+    setPaginationEned(10);
+
+  }
 
   const filter = (e) => {
     const keyword = e.target.value;
@@ -46,9 +80,134 @@ export default function Dashboard() {
 
   return (
     <div className={styles.main}>
-    <CsvDownloadButton data={filterData} className='p-0 m-4 w-40 h-10 text-[1.2rem] rounded' 
+       <span className="tex-fourth p-2 m-2 bg-red-600 mx-auto rounded-xl font-bold  text-fourth">
+        <p className="text-[1.3rem]">1- Tüm verileri API den çekmek istiyorsanız lütfen sayfayı yenileyin.</p>
+        <p className="text-[1.3rem]">2- Arama- guruplama - sayfalamaya göre csv alınmaktadır.</p>
+        <p className="text-[1.3rem]">3- Listelenen verilerin üzerine tıkladığınızda detaylar gözükmektedir.</p>
+        <p className="text-[1.3rem]">4- Düzenle butonuna basarak düzenleme penceresine ulaşabilir ve verileri güncelleyebilirsiniz.</p>
+        <p className="text-[1.3rem]">5- Güncelleme yaptıktan sonra arama bölümünden ülke ismine göre aratarak güncel veriye ulaşabilirsiniz. </p>
+        </span>
+      
+      <div className="w-full flex justify-center gap-10 items-center m-4 flex-wrap">
+      <button className='mt-2 p-0 w-80 h-10 text-[1.2rem] rounded' onClick={
+        () => {
+          setFilterData(allData);
+          setSearch('');
+          setGroupData([]);        
+          setPaginationStart(0);
+          setPaginationEned(10);
+        }
+      }>
+        Tabloyu Sıfırla
+      </button>
+        <button onClick={
+          () => pagination("prev")
+        } className='mt-2 p-0 w-40 h-10 text-[1.2rem] rounded'>{`önceki sayfa`}</button>
+        <button onClick={
+          () => pagination("next")
+        } className='mt-2 p-0 w-40 h-10 text-[1.2rem] rounded'>{`sonraki sayfa`
+        }</button>
+    <button className='mt-2 p-0 w-80 h-10 text-[1.2rem] rounded' onClick={
+      () => group()
+    }
+    >Seçilmiş gurup elemanlarını listele</button>
+    <CsvDownloadButton data={filterData} className='mt-2 p-0  w-40 h-10 text-[1.2rem] rounded' 
     filename='data.csv'
      >CSV Download</CsvDownloadButton>
+     </div>
+    <input
+        type="search"
+        value={search}
+        onChange={filter}
+        className="w-[60vw] px-4 py-4 mx-auto my-4 bg-fourt text-xl border rounded-md shadow-sm ring-white focus:outline-none ring-1 focus:ring-2 focus:ring-third focus:border-transparent"
+        placeholder="Ülke İsmine Göre Arayınız..."
+      />
+      <div id="editable" style={{display:"none"}} className="gap-4 justify-center items-center m-2 p-2 bg-third rounded-xl w-full flex-wrap mx-auto">
+        <input  id="editable_name" type="text" className=" px-4 py-4 mx-auto my-4 bg-second text-xl border rounded-md shadow-sm ring-white focus:outline-none ring-1 focus:ring-2 focus:ring-third focus:border-transparent"
+        placeholder={editIndex.name} />
+        <input id="editable_capital" type="text" className=" px-4 py-4 mx-auto my-4 bg-second text-xl border rounded-md shadow-sm ring-white focus:outline-none ring-1 focus:ring-2 focus:ring-third focus:border-transparent"
+        placeholder={editIndex.capital}/>
+        <input id="editable_population" type="text" className=" px-4 py-4 mx-auto my-4 bg-second text-xl border rounded-md shadow-sm ring-white focus:outline-none ring-1 focus:ring-2 focus:ring-third focus:border-transparent"
+        placeholder={editIndex.population}/>
+        <input id="editable_region" type="text" className=" px-4 py-4 mx-auto my-4 bg-second text-xl border rounded-md shadow-sm ring-white focus:outline-none ring-1 focus:ring-2 focus:ring-third focus:border-transparent"
+        placeholder={editIndex.region}/>
+        <input id="editable_borders" type="text" className=" px-4 py-4 mx-auto my-4 bg-second text-xl border rounded-md shadow-sm ring-white focus:outline-none ring-1 focus:ring-2 focus:ring-third focus:border-transparent"
+        placeholder={editIndex.borders}/>
+        
+        <button className="my-2 p-0 w-40 h-20 text-[1.4rem] rounded bg-black text-fourth" onClick={
+          () =>{ 
+            
+              const editable = document.getElementById("editable");
+              editable.style.display = "none";
+              
+              const name = document.getElementById("editable_name").value;
+              const capital = document.getElementById("editable_capital").value;
+              const population = document.getElementById("editable_population").value;
+              const region = document.getElementById("editable_region").value;
+              const borders = document.getElementById("editable_borders").value;
+              const flags = editIndex.flags;
+              const maps = editIndex.maps;
+              
+              const data = {
+                name: {
+                  common: name
+                },
+                flags:{
+                  png: flags
+                },
+                maps:{
+                  googleMaps: maps
+                },
+                capital: [capital],
+                population: population,
+                region: region,
+                borders: [borders]
+              }
+              if(name != "" && name != null && name != undefined){
+
+            setAllData((current) =>
+                  current.filter(
+                    (item) => item.name.common != editIndex.name
+                  )
+                );
+                    console.log(allData)
+                    setAllData((current) => [...current, data]);
+                    setFilterData((current) =>
+                  current.filter(
+                    (item) => item.name.common != editIndex.name
+                  )
+                );
+              }
+              else{
+                alert("Ülke İsmi Boş Bırakılamaz!")
+              }                
+            }
+
+            
+          }>Güncelle</button>
+          <button className="my-2 p-0 w-40 h-20 text-[1.4rem] rounded bg-black text-fourth" onClick={
+          () =>{ 
+            
+              const editable = document.getElementById("editable");
+              editable.style.display = "none";
+              
+            setAllData((current) =>
+                  current.filter(
+                    (item) => item.name.common != editIndex.name
+                  )
+                );
+                  setFilterData((current) =>
+                  current.filter(
+                    (item) => item.name.common != editIndex.name
+                  )
+                );
+
+                              
+            }
+
+            
+          }>Seçili Veriyi Sil</button>
+      </div>
     <div className={styles.table}>
       <div className={styles.table__head}>
       <span>Gurup Seçimi</span>
@@ -71,15 +230,21 @@ export default function Dashboard() {
                 }                   
                 else {
                   details.style.display = "none";
-                }
-                
-
-              }}
-              
+                }          
+              }}              
             >
               <div className={styles.table__body__title}               
               >
-              <input value={item.name.common} type="checkbox" onChange={e => console.log(e.target.value)}/>
+              <div className="flex gap-10 justify-center items-center">
+              <input value={item.name.common} type="checkbox" onChange={(e)=> setGroupData([...groupData, e.target.value])}/>
+              <button  style={{display:"block"}} className="mt-2 p-0 w-20 h-10 text-[1.2rem] text-third bg-first rounded cursor-help" 
+              onClick={(e)=>{
+                edit(e, index);
+                document.getElementById("editable").style.display = "flex";
+
+              }}
+              >Düzenle</button>
+              </div>
                 <span>{index+1}</span>
                 <span>{item.name.common}</span>
                 <span><img width={50} src={item.flags.png} alt="flag"/></span>
@@ -101,18 +266,6 @@ export default function Dashboard() {
         )        
       }      
       <div className={styles.table__body}>
-      </div>
-      <input
-        type="search"
-        value={search}
-        onChange={filter}
-        className="w-full px-4 py-4 mt-4 bg-second text-xl border rounded-md shadow-sm ring-white focus:outline-none ring-1 focus:ring-2 focus:ring-third focus:border-transparent"
-        placeholder="Ülke İsmine Göre Arayınız..."
-      />
-      <div className={styles.underbar}>
-      
-        <button onClick={() => pagination("prev")}>Önceki</button>
-        <button onClick={() => pagination("next")}>Sonraki</button>
       </div>
     </div>
     </div>
